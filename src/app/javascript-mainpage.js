0ec16d25c27cb6124e6afe4bccf441cd0db3d0c3
Aisumee <208887407+Aisumee@users.noreply.github.com>
2025-05-26 21:38:41 -0700

    2321
    312312312

--- src/app/javascript-mainpage.js
+++ src/app/javascript-mainpage.js
@@ -4,33 +4,33 @@ const CONTENT_SETS = {
     description:
       "The official government of the new world, maintaining order while striving to find a cure for Collapse Syndrome",
     images: [
-      "public/bg/pus-icon-removebg-preview.png",
-      "public/bg/scissors-icon-unclcikedremovebg-preview.png",
-      "public/bg/urbino-symbol-unclicked-removebg-preview.png",
+      "/public/bg/pus-icon-removebg-preview.png",
+      "/public/bg/scissors-icon-unclcikedremovebg-preview.png",
+      "/public/bg/urbino-symbol-unclicked-removebg-preview.png",
     ],
-    background: "url('public/bg/PUS_Wallpaper.jpg')",
+    background: "url('/public/bg/PUS_Wallpaper.jpg')",
   },
   2: {
     title: "SCISSORS",
     description:
       "An armed group opposing P.U.S is attempting to seize control of Bablo Crystals to open a portal back to the old world.",
     images: [
-      "public/bg/pus-icon-notclicked-removebg-preview.png",
-      "public/bg/scissors-icon-removebg-preview.png",
-      "public/bg/urbino-symbol-unclicked-removebg-preview.png",
+      "/public/bg/pus-icon-notclicked-removebg-preview.png",
+      "/public/bg/scissors-icon-removebg-preview.png",
+      "/public/bg/urbino-symbol-unclicked-removebg-preview.png",
     ],
-    background: "url('public/bg/scissors-wallpaper.jpg')",
+    background: "url('/public/bg/scissors-wallpaper.jpg')",
   },
   3: {
     title: "URBINO",
     description:
       "A commercial syndicate controlling the industries of the new world, exploring ways to reach other dimensions using crystal energy",
     images: [
-      "public/bg/pus-icon-notclicked-removebg-preview.png",
-      "public/bg/scissors-icon-unclcikedremovebg-preview.png",
-      "public/bg/urbino-icon.png",
+      "/public/bg/pus-icon-notclicked-removebg-preview.png",
+      "/public/bg/scissors-icon-unclcikedremovebg-preview.png",
+      "/public/bg/urbino-icon.png",
     ],
-    background: "url('public/bg/urbino-background-me.jpg')",
+    background: "url('/public/bg/urbino-background-me.jpg')",
   },
 };
 
